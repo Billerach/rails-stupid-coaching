a52b2206f9742d65bba41a36f802b247b0a6530b
Pascal Billerach <p.billerach@gmail.com>
2022-11-14 13:06:34 +0100

    LUNCH BREAK

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+  def index
+  end
+
+  def ask
+  end
+
+  def answer
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/index", to: "pages#index"
+
+  get "/ask", to: "pages#ask"
+
+  get "/answer", to: "pages#answer"
+
 end
